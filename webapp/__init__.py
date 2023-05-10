3e37b43b7efb84f50ae6b447b4465b7fe1e2febd
alex1983ovdoc1 <bondar1983ovdoc1@gmail.com>
2023-05-10 15:34:42 +0300

    Add model user and functional logining

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,10 +1,12 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 # from a2_weather import weather_by_city 
 
-from webapp.model import db, News
+from webapp.forms import LoginForm
+from webapp.model import db, News, User
 from webapp.weather_html import weather_by_city 
-# from webapp.python_news import get_python_news
+from webapp.python_news import get_python_news
 
 
 '''
@@ -20,6 +22,14 @@ def create_app():
     app.config.from_pyfile('config.py')     # load config.py -> .config + .from_pyfile
     db.init_app(app)                        # initialisation db object (App)
 
+    login_manager = LoginManager()          # create logMan's object
+    login_manager.init_app(app)             # initialisation object
+    login_manager.login_view = 'login'      # name function for this object
+
+    # gived user from DB
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
 
     @app.route('/')                                                 # start flask's object -> app
     def index():
@@ -29,4 +39,47 @@ def create_app():
         # news_list = get_python_news()
         news_list = News.query.order_by(News.published.desc()).all()                                           # go function news
         return render_template('index.html', page_title=title, weather = weather, news_list=news_list) # send to site
+
+
+    # user's form
+    @app.route('/login')
+    def login():
+        if current_user.is_authenticated:           # if user authenticated
+            return redirect(url_for('index'))       # go start page
+        title = 'Authorization'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+
+
+    # processing login
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+
+        if form.validate_on_submit():
+            user = User.query.filter(User.username == form.username.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('/ You going in site! /')
+                return redirect(url_for('index'))
+        flash('/ Name or password not correct /')
+        return redirect(url_for('login'))
+
+    # user logout
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('/ You logout from site. /')
+        return redirect(url_for('index'))
+
+    # page for admin
+    @app.route('/admin')
+    @login_required
+    def admin_index():
+        if current_user.is_admin:
+            return "Hello admin!"
+        else:
+            return "You aren't admin."
+
+
     return app
--- webapp/model.py
+++ webapp/model.py
@@ -1,4 +1,6 @@
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()                                   # create db's object
 
@@ -9,6 +11,28 @@ class News(db.Model):                                       # class News inherit
     published = db.Column(db.DateTime, nullable=False)      # column published
     text = db.Column(db.Text, nullable=True)                # text news (nullable=True -> None)
 
-
     def __repr__(self):
         return '<News {} {}>'.format(self.title, self.url)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(10), index=True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
+    
+
+    def __repr__(self):
+        return '<User name: {} id: {}>'.format(self.username, self.id)
+
+
